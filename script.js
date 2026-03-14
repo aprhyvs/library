a8c63e57fc9fae99f9d0b1bcf8aa55a31108331b
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2026-03-14 22:56:46 +0800

    add dialog modal

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const library = document.getElementById("library")
 const myLibrary = [];
+const bookshelf = document.getElementById("bookshelf");
+const addBookBtn = document.getElementById("add-book");
+const addBookForm = document.getElementById("add-book-form");
+const closeBookFormBtn = document.getElementById("close-modal");
 
 function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
@@ -8,22 +11,24 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.info = function() {
-    console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
+    console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
   }
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read))
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
-addBookToLibrary("MLBB Guide", "Roman", 69, true)
-addBookToLibrary("MLBB Guide II", "Greek", 420, false)
-addBookToLibrary("MLBB Guide III", "Egyptian", 1337, true)
+addBookToLibrary("MLBB Guide", "Roman", 69, true);
+addBookToLibrary("MLBB Guide II", "Greek", 420, false);
+addBookToLibrary("MLBB Guide III", "Egyptian", 1337, true);
+addBookToLibrary("MLBB Guide IV", "Viking", 300, true);
+addBookToLibrary("MLBB Guide V", "Turks", 67, true);
 
 //render each book unto page
 function renderBooks() {
   for (const book of myLibrary) {
-    library.insertAdjacentHTML("beforeend", 
+    bookshelf.insertAdjacentHTML("beforeend", 
       `
       <div class="book">
         <p class="book__title">${book.title}</p>
@@ -38,4 +43,13 @@ function renderBooks() {
 
 renderBooks()
 
-// console.log(myLibrary)
+function addBook() {
+  addBookForm.showModal();
+}
+
+addBookBtn.addEventListener("click", addBook);
+
+closeBookFormBtn.addEventListener("click", () => {
+  addBookForm.close();
+});
+
