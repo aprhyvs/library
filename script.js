52fc8e66018fe7b67778cbe7938efb2eb6f673c9
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2026-06-25 21:47:59 +0800

    change readBook() & deleteBook() as static methods

--- script.js
+++ script.js
@@ -15,8 +15,21 @@ class Book {
     this.read = read
   }
 
-  readBook() { 
-    this.read = !this.read
+  // we could make `getBookIndex` and `getBookToDelete` into one...
+  static readBook(element, elementBookIdParam) { 
+    const getBookIndex = Book.myLibrary.findIndex((book) => book.id === elementBookIdParam);
+    const bookToRead = Book.myLibrary[getBookIndex]
+
+    bookToRead.read = !bookToRead.read
+    renderBooks()
+  }
+
+  static deleteBook(element, elementBookIdParam) {
+    const getBookToDelete = Book.myLibrary.findIndex((book) => book.id === elementBookIdParam);
+
+    element.remove()
+    Book.myLibrary.splice(getBookToDelete, 1)
+    renderBooks()
   }
 }
 
@@ -24,20 +37,11 @@ function addBookToLibrary(title, author, pages, read) {
   Book.myLibrary.push(new Book(title, author, pages, read));
 }
 
-function deleteBook() {
-  const getBookToDelete = Book.myLibrary.findIndex((book) => book.id === this.book)
+function getBook(element, elementBookIdParam) {
+  const getBookIndex = Book.myLibrary.findIndex((book) => book.id === elementBookIdParam);
 
-  Book.myLibrary.splice(getBookToDelete, 1)
+  console.log(Book.myLibrary[getBookIndex])
 }
-
-function getBook(element) {
-  console.log(element)
-  console.log(element.dataset.id)
-}
-
-addBookToLibrary("titlehere", "joe", 69, true)
-addBookToLibrary("titlehere", "joe", 69, true)
-addBookToLibrary("titlehere", "joe", 69, true)
 
 function renderBooks() {
   bookshelf.innerHTML = ""
@@ -46,11 +50,12 @@ function renderBooks() {
     bookshelf.insertAdjacentHTML("beforeend",
     ` 
     <div class="book" data-id="${book.id}">
-      <button data-action="get-book">Get Book</button>
+      <button data-action="delete-book">Delete Book</button>
       <p class="book__title">${book.title}</p>
       <p class="book__author">${book.author}</p>
       <p class="book__pages">${book.pages}</p>
       <p class="book__read">${book.read}</p>
+      <button data-action="read-book">Read Book</button>
     </div>
     `
     );
@@ -59,10 +64,18 @@ function renderBooks() {
   const bookEl = document.querySelectorAll(`[data-id]`);
 
   bookEl.forEach(element => {
-    const getBookBtn = element.querySelector(`[data-action="get-book"]`);
+    const deleteBookBtn = element.querySelector(`[data-action="delete-book"]`);
+    const readBookBtn = element.querySelector(`[data-action="read-book"]`);
 
-    getBookBtn.addEventListener("click", () => getBook(element))
+    const elementBookId = element.dataset.id
+
+    deleteBookBtn.addEventListener("click", () => Book.deleteBook(element, elementBookId))
+    readBookBtn.addEventListener("click", () => Book.readBook(element, elementBookId))
   })
 }
+
+addBookToLibrary("titlehere", "joe", 69, true)
+addBookToLibrary("nottitle", "rikishi", 69, true)
+addBookToLibrary("thistitle", "jose", 69, true)
 
 renderBooks()
